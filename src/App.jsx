3c2fc0b34ff258e26a278d8b1fbc90e9a919621b
md-alif007 <mohammadalif043@gmail.com>
2026-03-04 15:39:55 +0600

    toggle & availabe and selected playersection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import AvailablePlayers from './components/AvailabelPlayers/AvailablePlayers'
+import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers';
 
 const fetchPlayersData = async () => {
   const res = await fetch("/players.json")
@@ -10,14 +11,39 @@ const fetchPlayersData = async () => {
 
 function App() {
 
+  const [toggle, setToggle] = useState(true);
+
   const playersPromise = fetchPlayersData();
 
   return (
     <>
       <Navbar></Navbar>
-      <Suspense fallback={<p>loading players data...</p>}>
-        <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
-      </Suspense>
+
+      <div className='mx-auto w-11/12 flex justify-between m-5'>
+        <h1 className='font-bold text-2xl'>Available players</h1>
+        <div className='font-bold'>
+          <button onClick={() => setToggle(true)}
+            className={`py-3 px-4 border border-gray-400 rounded-l-2xl border-r-0 
+            ${toggle === true ? "bg-[#E7FE29]" : ""}`}>
+            Available
+          </button>
+          <button onClick={() => setToggle(false)}
+            className={`py-3 px-4 border border-gray-400 rounded-r-2xl border-l-0
+            ${toggle === false ? "bg-[#E7FE29]" : ""}`}>
+            Selected {0}
+          </button>
+        </div>
+      </div>
+
+      {
+        toggle === true ?
+          <Suspense fallback={<p>loading players data...</p>}>
+            <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
+          </Suspense> :
+          <SelectedPlayers></SelectedPlayers>
+      }
+
+
     </>
   )
 }
